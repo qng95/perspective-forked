b5d36f353c16260090d1149f0742327643ca6d90
Andrew Stein <steinlink@gmail.com>
2022-05-30 22:38:49 -0400

    Remove webpack

--- python/perspective/setup.py
+++ python/perspective/setup.py
@@ -289,7 +289,7 @@ data_files_spec = [
     (
         "share/jupyter/nbextensions/@finos/perspective-jupyterlab",
         "perspective/nbextension/static",
-        "*.js*",
+        "*.*",
     ),
     # Activate nbextension by default
     (
