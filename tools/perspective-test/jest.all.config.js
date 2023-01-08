4d3959b9a6468e243153e2d2bda5968764e7f866
Andrew Stein <steinlink@gmail.com>
2023-01-08 11:37:34 -0500

    Remove `babel-jest` and inline record reporting

--- tools/perspective-test/jest.all.config.js
+++ tools/perspective-test/jest.all.config.js
@@ -8,15 +8,17 @@ module.exports = {
         "packages/perspective-jupyterlab/test/js",
     ],
     verbose: true,
-    testURL: "http://localhost/",
+    testEnvironmentOptions: {
+        url: "http://localhost/",
+    },
     transform: {
-        ".js$": "@finos/perspective-test/src/js/transform.js",
         ".html$": "html-loader-jest",
     },
     collectCoverage: true,
     collectCoverageFrom: ["packages/perspective/dist/cjs/**"],
     coverageProvider: "v8",
     coverageReporters: ["cobertura", "text"],
+
     // perspective-jupyterlab tests mock `@jupyter-widgets`, which is in
     // Typescript.
     transformIgnorePatterns: [
@@ -24,11 +26,7 @@ module.exports = {
     ],
     automock: false,
     setupFiles: ["@finos/perspective-test/src/js/beforeEachSpec.js"],
-    reporters: [
-        "default",
-        "@finos/perspective-test/src/js/reporter.js",
-        "jest-junit",
-    ],
+    reporters: ["default", "jest-junit"],
     globalSetup: "@finos/perspective-test/src/js/globalSetup.js",
     globalTeardown: "@finos/perspective-test/src/js/globalTeardown.js",
 };
